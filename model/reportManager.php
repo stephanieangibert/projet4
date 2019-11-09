5da6c1333cfa8de3c4f9b99e6f5ec359142afc9a
stephanie angibert <stephanieangibert@orange.fr>
2019-11-09 05:23:03 +0100

    debug report comment

--- model/reportManager.php
+++ model/reportManager.php
@@ -5,7 +5,7 @@ class ReportManager extends Manager
     public function reporting($id)
 {  
     $db = $this->dbConnect();   
-    $reporting=$db->prepare('UPDATE comments SET reporting=reporting+1 WHERE id = ?"');
+    $reporting=$db->prepare('UPDATE comments SET reporting=reporting+1 WHERE id = ?');
     $reportCom=$reporting->execute(array($id));
     return $reportCom;
 }
